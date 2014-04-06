83e4c35b3004ec938002e31a92ad9d58466a0376
Kyndt Clovis <clovis.kyndt@epitech.eu>
2014-04-06 21:08:19 +0200

    add bootstrap framework css

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -26,15 +26,16 @@ class HomeController < ApplicationController
 
   def index
     @link_menu = Array.new()
-    @link_menu.push(InformationsMenu.new("Ameliorations",  "ameliorations"))
+    @link_menu.push(InformationsMenu.new("Races",   "depandancies"))
     @link_menu.push(InformationsMenu.new("Buildings",      "buildings"))
-    @link_menu.push(InformationsMenu.new("Capacities",     "capacities"))
-    @link_menu.push(InformationsMenu.new("Depandancies",   "depandancies"))
-    @link_menu.push(InformationsMenu.new("Effects",        "effects"))
-    @link_menu.push(InformationsMenu.new("Evenements",     "evenements"))
-    @link_menu.push(InformationsMenu.new("Objects",        "objects"))
-    @link_menu.push(InformationsMenu.new("Projectiles",    "projectiles"))
-    @link_menu.push(InformationsMenu.new("Spells",         "spells"))
     @link_menu.push(InformationsMenu.new("Units",          "units"))
+    @link_menu.push(InformationsMenu.new("Capacities",     "capacities"))
+    @link_menu.push(InformationsMenu.new("Projectiles",    "projectiles"))
+    @link_menu.push(InformationsMenu.new("Objects",        "objects"))
+    @link_menu.push(InformationsMenu.new("Effects",        "effects"))
+    @link_menu.push(InformationsMenu.new("Ameliorations",  "ameliorations"))
+    @link_menu.push(InformationsMenu.new("Spells",         "spells"))
+    @link_menu.push(InformationsMenu.new("Evenements",     "evenements"))
+    @link_menu.push(InformationsMenu.new("Map",            "map"))
   end
 end
